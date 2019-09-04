578c8836aefab1dbfdc3566ff4fd9d3cd025bef6
Felipe Gomes <felipegomesilva@gmail.com>
2019-09-04 13:20:26 -0300

    .

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -7,7 +7,7 @@ def call() {
 
     if (p.buildImage == true) {
         stage('Build') {
-         docker.build(p.imageName:"${JOB_NUMBER}")
+         docker.build(p.imageName:"${env.BUILD_ID}")
           sh 'docker images'
        }       
   }
