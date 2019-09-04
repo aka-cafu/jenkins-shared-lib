7d98dc13686d900b72fbc48c95878466689d3842
Felipe Gomes <felipegomesilva@gmail.com>
2019-09-04 16:13:04 -0300

    .

--- vars/awsEC2.groovy
+++ vars/awsEC2.groovy
@@ -20,7 +20,7 @@ def call() {
    checkout scm
   }
   def values = terraformAwsEc2()
-  if (values.terraformVersion < 0.10 ) {
+  if (values.terraformVersion <= 0.11 ) {
    stage('Version') {
     dir(values.ec2Module) {
        echo "Version unsupported!"
