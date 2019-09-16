bf096678dfe99db31613f2e162973f628e03eb8e
Felipe Gomes <felipegomesilva@gmail.com>
2019-09-16 14:36:42 -0300

    terraform init var modified

--- vars/terraformInit.groovy
+++ vars/terraformInit.groovy
@@ -2,11 +2,7 @@ def call() {
   def values = terraformAwsEc2()
   if (params.ENVIRONMENT == 'PROD') {
      sh "terraform init -backend-config='${values.s3Bucket}' -backend-config='key=application/${params.NAME}-${params.TAG}/terraform.tfstate' -backend-config='region=${values.awsRegion}' && sed -i 's/appname/${params.USERDATA}/g' main.tf"
-    }
-   }
   } else {
-   dir(values.ec2Module) {
     sh "terraform init -backend-config='bucket=${values.s3BucketDevHom}' -backend-config='key=application/${params.NAME}-${params.TAG}/terraform.tfstate' -backend-config='region=${values.awsRegionDevHom}' && sed -i 's/appname/${params.USERDATA}/g' main.tf"
-   }
   }
 }
