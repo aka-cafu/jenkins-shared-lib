a03d508ade8b1a8e9b48382c47bdbb509a4e23f7
Felipe Gomes <felipegomesilva@gmail.com>
2019-09-16 15:51:44 -0300

    var modified

--- vars/terraformApplyEc2.groovy
+++ vars/terraformApplyEc2.groovy
@@ -1,11 +1,7 @@
 def call() {
  def values = terraformAwsEc2()
  dir(values.ec2Module) {
-  when {
-   expression {
-    return !(params.DELETE)
-   }
-  } {
+  if !(params.DELETE) {
    def terraformApprove = input message: 'Do you really want to create the resources described above?',
     parameters: [choice(name: 'Apply', choices: 'yes\nno', description: 'Enter a value')]
    if (terraformApprove == "yes") {
